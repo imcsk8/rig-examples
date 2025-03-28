3959f3c6f2c5f764414c98487344a7dae45c7641
Iván Chavero <ichavero@chavero.com.mx>
2025-03-28 09:55:26 -0600

    Move context stuff to its own file

---
+++ k8s-ai-operator/src/context.rs
@@ -0,0 +1,20 @@
+use kube::client::Client;
+
+/// ContextData just wraps `kube::client::Client` so it can be added implementations
+/// Context injected with each `reconcile` and `on_error` method invocation.
+pub struct ContextData {
+    /// Kubernetes client to make Kubernetes API requests with. Required for K8S resource management.
+    client: Client,
+}
+
+/// Context data handler
+impl ContextData {
+    /// Constructs a new instance of ContextData.
+    ///
+    /// # Arguments:
+    /// - `client`: A Kubernetes client to make Kubernetes REST API requests with. Resources
+    /// will be created and deleted with this client.
+    pub fn new(client: Client) -> Self {
+        ContextData { client }
+    }
+}

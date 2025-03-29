81378f6439cb32ed503ab4f2d8273f10811efd09
Iván Chavero <ivan.chavero@ugd.gob.mx>
2025-03-29 12:04:48 -0600

    Added user input error

--- k8s-ai-operator/src/error.rs
+++ k8s-ai-operator/src/error.rs
@@ -4,13 +4,15 @@ use thiserror::Error;
 /// Enum for managing different types of errors, needed because the reconciler run function
 /// needs to implement StdError
 #[derive(Debug, Error)]
-pub enum OperatorError {
+pub enum AiOperatorError {
     /// Errors reported by the kube-rs crate
     #[error("Kubernetes Example Operator Error: {source}")]
     KubeError {
         #[from]
         source: kube::Error,
     },
-    // TODO: add more types of errors if needed
+    /// Error in user input or AiOperator resource definition, typically missing fields.
+    #[error("Invalid AiOperator CRD: {0}")]
+    UserInputError(String),
 }
 
